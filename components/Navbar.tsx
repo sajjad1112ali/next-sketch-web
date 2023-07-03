81237f4f0fb0cc71e3451119bf9d8063686af1c9
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-07-03 12:48:24 +0500

    Added nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 function Navbar() {
+    const session = {};
   return (
-    <div>Navbar</div>
-  )
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.svg"}
+            width={0}
+            height={0}
+            className={"logoImage"}
+            alt="Sketch Haven"
+          />
+        </Link>
+        <ul className="xl:flex hidden gap-7">
+          {NavLinks.map((link, i) => (
+            <Link href={link.href} key={i}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {
+            session ? (<>USER PHOTO </>) : (<AuthProviders />)
+        }
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+export const NavLinks = [
+    { href: '/', key: 'Portrait Gallery', text: 'Portrait Gallery' },
+    { href: '/', key: 'Find Projects', text: 'Find Projects' },
+    { href: '/', key: 'Learn Development', text: 'Learn Development' },
+    { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
+    { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
+  ];
+
+  export const categoryFilters = [
+    "Portrait Gallery",
+    "Nature Sketches",
+    "Urban Landscapes",
+    "Abstract Expressions",
+    "Comic Creations",
+    "Fantasy Realms",
+    "Botanical Beauty",
+    "Character Designs",
+    "Historical Sketches",
+  ]
+
+  
+  
+  
+  
+  
+  
