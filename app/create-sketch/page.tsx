320009833294088e6d0ff2a2ceb686db6efc9022
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-07-10 16:11:20 +0500

    Added category dropdown

--- app/create-sketch/page.tsx
+++ app/create-sketch/page.tsx
@@ -1,11 +1,15 @@
 import Modal from "@/components/Modal";
 import SketchForm from "@/components/SketchForm";
+import { getCurrentUser } from "@/lib/session";
+import { redirect } from "next/navigation";
 
-const createSketch = () => {
+const createSketch = async () => {
+  const session = await getCurrentUser();
+  if(!session?.user) redirect('/')
   return (
     <Modal>
       <h3 className="modal-head-text">Create a new project</h3>
-      <SketchForm />
+      <SketchForm type='create' session={session} />
     </Modal>
   );
 };
--- components/SketchForm.tsx
+++ components/SketchForm.tsx
@@ -1,7 +1,72 @@
-const SketchForm = () => {
-  return (
-    <div>SketchForm</div>
-  )
-}
+"use client";
+import { SessionInterface } from "@/common.types";
+import { categoryFilters } from "@/constants";
+import Image from "next/image";
+import React, { ChangeEvent } from "react";
+import CategoriesDD from "./CategoriesDD";
+import FormField from "./FormField";
 
-export default SketchForm
+type Props = {
+  type: string;
+  session: SessionInterface;
+};
+const SketchForm = ({ type, session }: Props) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+  const handleStateChange = (fieldName: string, value: string) => {};
+  const form = {
+    image: "",
+    title: "",
+    description: "",
+    category: "",
+  };
+  return (
+    <form onSubmit={handleFormSubmit} className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && "Chose a poster for you sketch"}
+        </label>
+        <input
+          type="file"
+          id="image"
+          accept="image/*"
+          required={type === "create"}
+          onChange={handleChangeImage}
+          className="form_image-input"
+        />
+        {form?.image && (
+          <Image
+            src={form.image}
+            className="sm:p-10 object-contain z-20"
+            fill
+            alt="Sketch poster"
+          />
+        )}
+      </div>
+      <FormField
+        title="Title"
+        state={form.title}
+        placeHolder="Something"
+        setState={(value) => handleStateChange("title", value)}
+      />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeHolder="Explain the characteristics of your sketch"
+        setState={(value) => handleStateChange("description", value)}
+        isTextArea={true}
+      />
+      <CategoriesDD
+        title='Category'
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
+      />
+      <div className="flexStart w-full">
+        <button>Create</button>
+      </div>
+    </form>
+  );
+};
+
+export default SketchForm;
