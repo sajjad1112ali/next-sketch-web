9129092f8327253c722cb70a08f6b3031eb05c1f
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-07-13 16:10:15 +0500

    Added fetch sketch

--- lib/db/sketch-actions.ts
+++ lib/db/sketch-actions.ts
@@ -1,5 +1,23 @@
 import prisma from "@/lib/db/prisma";
-export const getSkethes =async () => {
-    const sketches = await prisma.sketch.findMany();
-      return sketches;
-}
+export const getSkethes = async () => {
+  const sketches = await prisma.sketch.findMany();
+  return sketches;
+};
+
+export const getSketheDetails = async (id: string) => {
+  const sketch = await prisma.sketch.findUnique({
+    where: {
+      id: parseInt(id),
+    }
+  });
+  return sketch;
+};
+
+export const getUserProjects = async (userId: number) => {
+  const sketch = await prisma.sketch.findMany({
+    where: {
+      userId,
+    }
+  });
+  return sketch;
+};
