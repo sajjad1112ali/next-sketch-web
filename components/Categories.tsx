06e1bdb223bbe981996bca796308e16512a72ed1
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-07-18 15:53:09 +0500

    Active category filter

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -7,11 +7,22 @@ const Categories = () => {
   const router = useRouter();
   const pathName = usePathname();
   const searchParams = useSearchParams();
+  const filter = searchParams.get('category');
+  const handleTag = (category: string) => {
+    router.push(`${pathName}?category=${category}`)
+  };
   return (
     <div className="flexBetween w-full flex-wrap overflow-auto">
       <ul className="flex gap-5 shrink-0">
         {categoryFilters.map((category: string) => (
-          <button key={category}>{category}</button>
+          <button
+            key={category}
+            type="button"
+            onClick={() => handleTag(category)}
+            className={`${category === filter ? 'bg-light-white-300 font-medium' : 'font-normal'} px-4 py-3 capitalize rounded-lg whitespace-nowrap`}
+          >
+            {category}
+          </button>
         ))}
       </ul>
     </div>
